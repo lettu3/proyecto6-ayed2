2e3cb03aed3122199c58c732f058548562cecd2d
igna <ignas.gomezb12@gmail.com>
2023-06-06 22:44:35 -0300

    ej1 falta implentar remove, ej2 terminado, ej3 incompleto

--- ej3/dict.c
+++ ej3/dict.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdlib.h>
 #include "dict.h"
 #include "key_value.h"
 
@@ -10,24 +11,63 @@ struct _node_t {
 };
 
 static bool invrep(dict_t d) {
-    /* needs implementation */
-    return true;
+    bool inv = false;
+    if (inv != NULL){
+        if(d->key == NULL || (d->left == NULL && d->right == NULL)){
+            inv = true;
+        }
+        else if(d->right = NULL){
+            inv = inv && invrep(d->left) && key_less(d->left->key, d->key);
+        }
+        else if(d->left = NULL){
+            inv = inv && invrep(d->right) && !key_less(d->right->key, d->key);
+        }
+        else{
+            inv = inv && invrep(d->left) && invrep(d->right) && key_less(d->left->key, d->key) && !key_less(d->right->key, d->key);
+        }
+    }
+    return inv;
 }
 
 dict_t dict_empty(void) {
     dict_t dict = NULL;
-    /* needs implementation */
+    dict = malloc(sizeof(struct _node_t));
+    dict->left = NULL;
+    dict->right = NULL;
+    dict->key = NULL;
+    dict->value = NULL;
+
+    assert(dict != NULL && (dict_length(dict) == 0u));
     return dict;
 }
 
 dict_t dict_add(dict_t dict, key_t word, value_t def) {
-    /* needs implementation */
+    
+    if(dict_exists(dict, word)){
+        if (key_eq(dict->key, word)){
+            free(dict->value);
+            dict->value = NULL;
+            dict->value = def;
+        }
+        else if (key_less(dict->key, word)){
+            dict->left = dict_add(dict->left, word, def);
+        }
+        else{
+            dict->right = dict_add(dict->right, word, def);
+        }
+    }
+    else{
+        if (dict->key == NULL){
+            dict->key = word;
+            dict->value = def;
+        }
+    }
     return dict;
 }
 
 value_t dict_search(dict_t dict, key_t word) {
     key_t def=NULL;
-    /* needs implementation */
+    
     return NULL;
 }
 
